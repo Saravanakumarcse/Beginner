d81dce31402504145a917c33ff342e15a769023b
Saravanakumarcse <manosaravanan31@gmail.com>
2016-10-16 01:55:38 -0700

    Create CheckChar.java

---
+++ CheckChar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class CheckChar
+{
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+System.out.println("No of Char in given string "+s.length());
+s.close();
+}
+}
