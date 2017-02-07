39a21e02cccff16e9e8fee2973ab01902acbf32e
Saravanakumarcse <manosaravanan31@gmail.com>
2017-02-07 09:19:06 -0800

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EvenOrOdd
+{
+public static void main(String args[])
+{
+int n;
+Scanner in=new Scanner(System.in);
+if(n%2==0)
+{
+System.out.println("Even");
+}
+else if(n%2!=0)
+{
+System.out.println("Odd");
+}
+}
